8952db1866db1b9527e147eed1ead8a4056178e6
acruzr <acruzr135@gmail.com>
2020-04-27 21:57:08 -0500

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'service-web-app';
+  title = 'service-web-app-test1';
 }
